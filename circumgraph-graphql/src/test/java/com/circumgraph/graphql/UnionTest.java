19ee72355fb6ce2723ef4a17fe0486d8843ecf6c
Andreas Holstenson <a@holstenson.se>
2021-07-26 07:47:57 +0200

    test(graphql): Add tests for switching type in union

--- circumgraph-graphql/src/test/java/com/circumgraph/graphql/UnionTest.java
+++ circumgraph-graphql/src/test/java/com/circumgraph/graphql/UnionTest.java
@@ -65,4 +65,118 @@ public class UnionTest
 		assertThat(result.pick("storeTest", "value", "__typename"), is("A"));
 		assertThat(result.pick("storeTest", "value", "valueA"), is("valueInUnion"));
 	}
+
+	@Test
+	public void testUpdateSameType()
+	{
+		var mutation = """
+			mutation($id: ID, $m: TestMutationInput!) {
+				storeTest(id: $id, mutation: $m) {
+					id,
+					value {
+						__typename
+
+						... on A {
+							valueA
+						}
+					}
+				}
+			}
+		""";
+
+		var result = execute(
+			mutation,
+			Maps.immutable.of(
+				"m", Maps.immutable.of(
+					"value", Maps.immutable.of(
+						"A", Maps.immutable.of(
+							"valueA", "valueInUnion"
+						)
+					)
+				)
+			)
+		);
+
+		result.assertNoErrors();
+
+		var id = result.pick("storeTest", "id");
+
+		result = execute(
+			mutation,
+			Maps.immutable.of(
+				"id", id,
+				"m", Maps.immutable.of(
+					"value", Maps.immutable.of(
+						"A", Maps.immutable.of(
+							"valueA", "updatedValueInUnion"
+						)
+					)
+				)
+			)
+		);
+
+		result.assertNoErrors();
+
+		assertThat(result.pick("storeTest", "value", "__typename"), is("A"));
+		assertThat(result.pick("storeTest", "value", "valueA"), is("updatedValueInUnion"));
+	}
+
+	@Test
+	public void testUpdateSwitchType()
+	{
+		var mutation = """
+			mutation($id: ID, $m: TestMutationInput!) {
+				storeTest(id: $id, mutation: $m) {
+					id,
+					value {
+						__typename
+
+						... on A {
+							valueA
+						}
+
+						... on B {
+							valueB
+						}
+					}
+				}
+			}
+		""";
+
+		var result = execute(
+			mutation,
+			Maps.immutable.of(
+				"m", Maps.immutable.of(
+					"value", Maps.immutable.of(
+						"A", Maps.immutable.of(
+							"valueA", "valueInUnion"
+						)
+					)
+				)
+			)
+		);
+
+		result.assertNoErrors();
+
+		var id = result.pick("storeTest", "id");
+
+		result = execute(
+			mutation,
+			Maps.immutable.of(
+				"id", id,
+				"m", Maps.immutable.of(
+					"value", Maps.immutable.of(
+						"B", Maps.immutable.of(
+							"valueB", 100
+						)
+					)
+				)
+			)
+		);
+
+		result.assertNoErrors();
+
+		assertThat(result.pick("storeTest", "value", "__typename"), is("B"));
+		assertThat(result.pick("storeTest", "value", "valueB"), is(100));
+	}
 }
