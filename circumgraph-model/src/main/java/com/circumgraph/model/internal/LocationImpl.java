97571c58c7e5dbb9ff878766b8b71652e484d674
Andreas Holstenson <a@holstenson.se>
2021-08-29 08:50:18 +0200

    chore(model): Support for scoping to a specific location

--- circumgraph-model/src/main/java/com/circumgraph/model/internal/LocationImpl.java
+++ circumgraph-model/src/main/java/com/circumgraph/model/internal/LocationImpl.java
@@ -7,6 +7,8 @@ import com.circumgraph.model.Location;
 public class LocationImpl
 	implements Location
 {
+	private static final ThreadLocal<Location> CURRENT = new ThreadLocal<>();
+
 	public static final Location UNKNOWN = new LocationImpl("Unknown Location");
 
 	private final String message;
@@ -42,5 +44,49 @@ public class LocationImpl
 		if(getClass() != obj.getClass()) return false;
 		LocationImpl other = (LocationImpl) obj;
 		return Objects.equals(message, other.message);
+	}
+
+	public static Location code()
+	{
+		StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+		for(int i=0; i<trace.length; i++)
+		{
+			String module = trace[i].getModuleName();
+			String className = trace[i].getClassName();
+			if(! "java.base".equals(module) && ! className.contains("com.circumgraph.model"))
+			{
+				return new LocationImpl(trace[i].toString());
+			}
+		}
+
+		return LocationImpl.UNKNOWN;
+	}
+
+	public static Location automatic(Location picked)
+	{
+		if(picked != null)
+		{
+			return picked;
+		}
+
+		var current = CURRENT.get();
+		return current == null ? code() : current;
+	}
+
+	public static Handle scope(Location location)
+	{
+		var current = CURRENT.get();
+		CURRENT.set(location);
+
+		return () -> {
+			if(current != null)
+			{
+				CURRENT.set(current);
+			}
+			else
+			{
+				CURRENT.remove();
+			}
+		};
 	}
 }
